79de374926637836ca931b7ca8a8dfd0b6571770
mmargutti <m.margutti@gmail.com>
2014-01-23 09:31:39 -0200

    Testanto commit

--- DistanceMatriz/TDistMat.h
+++ DistanceMatriz/TDistMat.h
@@ -13,7 +13,7 @@ class TDistMat
         bool isValidIndex(int i, int j); // Verifica se o índice é válido
         int convertIndex(int i, int j); // Se i e j forem válidos, converte o i e j para o índice correto no vetor
     public:
-        TDistMat(int ordem); // Construtor do TAD
+        TDistMat(int ordem); // Construtor do TAD alteração !
         float getDist(int i, int j); // Consulta a distância na posição i e j
         void setDist(int i, int j, float dist); // Atribui uma distância à posição i e j
         void print(); // Imprime a matriz
